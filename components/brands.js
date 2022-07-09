5a4105e0c0ac9d69a3e4d96c3be47f1e03cad85b
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-07-09 13:13:24 +0530

    tidal

--- components/brands.js
+++ components/brands.js
@@ -35,14 +35,7 @@ export default function Brands(brands) {
                         className="absolute inset-0 w-full h-full object-center border-gray-200"
                     />
                 </div>
-                <div className="px-3">
-                    <Scope_image
-                        src='/Brands/Mitsubishi-logo-238x238.png'
-                        height='100'
-                        width='100'
-                        className="absolute inset-0 w-full h-full object-center border-gray-200"
-                    />
-                </div>
+                
                 <div className="px-3">
                     <Scope_image
                         src='/Brands/hyundai-logo-238x238.png'
